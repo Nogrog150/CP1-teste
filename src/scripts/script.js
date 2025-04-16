0d58fe3c2092ad2b5e622bb7eca43c503694b3e6
Mauro <maurinho1402@hotmail.com>
2025-04-16 15:54:48 -0300

    Initial commit

---
+++ src/scripts/script.js
@@ -0,0 +1,6 @@
+const btn_navbar = document.querySelector(".btn_navbar")
+const nav = document.querySelector(".nav")
+
+btn_navbar.addEventListener("click", () => 
+  nav.classList.toggle("active")
+) 
